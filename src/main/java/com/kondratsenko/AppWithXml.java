f6dc60769182b567c9811889eaa0f7d8046f2297
maksim.kondratsenko <maxkj@yandex.ru>
2021-12-03 15:51:36 +0300

    Finished has been general beans with model

--- src/main/java/com/kondratsenko/AppWithXml.java
+++ src/main/java/com/kondratsenko/AppWithXml.java
@@ -1,12 +1,15 @@
 package com.kondratsenko;
 
 import com.kondratsenko.model.Zoo;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AppWithXml {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        //context.registerShutdownHook();
         Zoo myZoo = context.getBean("zoo", Zoo.class);
         myZoo.print();
+        context.close();
     }
 }
